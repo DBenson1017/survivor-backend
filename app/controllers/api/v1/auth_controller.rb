c21928eb13ba1eacb55099771314cb6838e52f65
DBenson1017 <devinbenson@gmail.com>
2020-09-25 08:57:58 -0400

    initial commit

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::AuthController < ApplicationController
+
+    def create 
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: {user: UserSerializer.new(@user)}, status: :created
+        else 
+            render json: { error: 'failed to create user'}, status: :not_acceptable
+        end 
+    end 
+
+    # def index 
+    #     @users = User.all
+    #     render json: @users
+    # end 
+
+    # def show
+    #     @user = User.find(params[:id])
+    #     render json: @user
+    # end 
+
+    private 
+    def user_params 
+        params.require(:user).permit(:username, :password, :first_name, :last_name, :email, :zip)
+    end
+end
