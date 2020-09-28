c617b2e6625d2fa5b6672f56abc8f90e6b5a0ed4
DBenson1017 <devinbenson@gmail.com>
2020-09-28 18:31:15 -0400

    Air class generate method functional

--- app/models/air.rb
+++ app/models/air.rb
@@ -7,8 +7,9 @@ require 'dotenv/load'
 class Air < ApplicationRecord
   belongs_to :location
 
-  def self.generate()
-    response = Unirest.get "https://us-air-quality-by-zip-code.p.rapidapi.com/getairqualityzipcode?zip=94111",
+  def self.generate(zip)
+    
+    response = Unirest.get "https://us-air-quality-by-zip-code.p.rapidapi.com/getairqualityzipcode?zip=#{zip}",
     headers:{
       "X-RapidAPI-Host" => "us-air-quality-by-zip-code.p.rapidapi.com",
       "X-RapidAPI-Key"=> ENV['AIR_QUALITY']
