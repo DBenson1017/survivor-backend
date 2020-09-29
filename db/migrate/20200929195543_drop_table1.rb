5fba06ae98d48459a42ce9d1076afdd0cb9059a3
DBenson1017 <devinbenson@gmail.com>
2020-09-29 15:57:08 -0400

    Tuesday PM

---
+++ db/migrate/20200929195543_drop_table1.rb
@@ -0,0 +1,5 @@
+class DropTable1 < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :locations_users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_003214) do
+ActiveRecord::Schema.define(version: 2020_09_29_195543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
