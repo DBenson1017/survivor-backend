8edc7881ed90bc3fddd087e5b8b87cef1d7ba669
DBenson1017 <devinbenson@gmail.com>
2020-10-01 15:01:14 -0400

    after bug fixes on user create / login on front end

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,18 +1,19 @@
-# class UserSerializer < ActiveModel::Serializer
+
 class UserSerializer < ActiveModel::Serializer
-    include FastJsonapi::ObjectSerializer 
+    # include FastJsonapi::ObjectSerializer 
 
     attributes :id, :username, :first_name, :last_name, :email, :zip
     has_many :favorites
 
-    # attribute :favorites do |user|
-    #   favorites_array = []
-    #   user.favorites.each do |favorite|
-    #     obj = {'location_id' => favorite.location_id}
+  end
+  
+  # attribute :favorites do |user|
+  #   favorites_array = []
+  #   user.favorites.each do |favorite|
+  #     obj = {'location_id' => favorite.location_id}
 
-    #     favorites_array << obj 
-    #   end 
-        
-    #     favorites_array
-    # end 
-end
+  #     favorites_array << obj 
+  #   end 
+      
+  #     favorites_array
+  # end 
